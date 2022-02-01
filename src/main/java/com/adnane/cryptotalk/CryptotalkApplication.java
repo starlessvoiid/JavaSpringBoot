3a4581919d47cc0519b499c016a77d9e210294b5
Adnane Slimani <60299177+Pstar-Adnane@users.noreply.github.com>
2022-02-01 21:28:02 +0100

    update

---
+++ src/main/java/com/adnane/cryptotalk/CryptotalkApplication.java
@@ -0,0 +1,13 @@
+package com.adnane.cryptotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adnane/cryptotalk/CryptotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adnane.cryptotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
