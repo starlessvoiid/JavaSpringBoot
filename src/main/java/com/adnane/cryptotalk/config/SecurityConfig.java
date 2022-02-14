d89d388190f3fe3b9b9a50e2d35bcd96e4bb1ae7
Adnane Slimani <60299177+Pstar-Adnane@users.noreply.github.com>
2022-02-14 15:53:48 +0100

    post get psswd encrypt

--- src/main/java/com/adnane/cryptotalk/config/SecurityConfig.java
+++ src/main/java/com/adnane/cryptotalk/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.adnane.cryptotalk.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,5 +18,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/adnane/cryptotalk/service/AuthService.java
@@ -0,0 +1,31 @@
+package com.adnane.cryptotalk.service;
+
+import com.adnane.cryptotalk.dto.RegisterRequest;
+import com.adnane.cryptotalk.model.User;
+import com.adnane.cryptotalk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUserName(registerRequest.getUsername());
+        user.setPassword(encodePassword(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        userRepository.save(user);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+}
